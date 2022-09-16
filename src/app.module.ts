2404a84c1f202056b65b8ccbb7a7d4a8d5371a44
T34Virus <treindle@gmail.com>
2022-09-16 10:48:59 -0700

    Add: app controller/service to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/controllers/users/users.controller';
 import { UsersService } from './users/services/users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'testuser123',
+      database: 'nestjs_mysql_tutorial',
+      entities: [],
+      synchronize: true,
+
+    })
+  ],
+  controllers: [
+    AppController,
+    UsersController
+  ],
+  providers: [
+    AppService,
+    UsersService
+  ],
 })
 export class AppModule {}
